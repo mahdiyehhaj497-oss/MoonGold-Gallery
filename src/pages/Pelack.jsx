5a0dc38378be0d83d467250ff0601c5863b23be5
mahdiyeh <mahdiyehhaj497@gmail.com>
2026-07-16 22:14:42 +0330

    add pages

---
+++ src/pages/Pelack.jsx
@@ -0,0 +1,7 @@
+export default function Pelack() {
+    return (
+        <>
+        <div>Pelack</div>
+        </>
+    )
+}
---
+++ src/pages/إracelet.jsx
@@ -0,0 +1,7 @@
+export default function Bracelet() {
+    return (
+      <>
+        <div>bracelet</div>
+      </>
+    );
+}
