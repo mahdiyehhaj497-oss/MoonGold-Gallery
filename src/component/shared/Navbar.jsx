6d46b77f74dc8cdd879dee5fe9b542e89b8825e6
mahdiyeh <mahdiyehhaj497@gmail.com>
2026-07-19 11:21:23 +0330

    add some changes

--- src/component/shared/Navbar.jsx
+++ src/component/shared/Navbar.jsx
@@ -1,15 +1,27 @@
+import { Link } from "react-router-dom";
 
 
 export default function Navbar() {
     return (
       <>
-        <div className="flex justify-center items-center ">
-          <span>
-            صفحه اصلی
-          </span>
-          <span>ارتباط با ما </span>
-          <span>سبد خرید</span>
-          <span>ثبت نام</span>
+        <div className="flex justify-center items-center  mt-6 bg-amber-100 py-2 mx-5 rounded-3xl">
+          <div className="mr-auto">
+            <span className="ml-6">قیمت طلا</span>
+          </div>
+          <div className="ml-auto gap-5">
+            <Link to="/singin">
+              <span className="mr-6">ثبت نام</span>
+            </Link>
+            <Link to="/contact">
+              <span className="mr-6">ارتباط با ما </span>
+            </Link>
+            <Link to="/paymentpage">
+              <span className="mr-6">سبد خرید</span>
+            </Link>
+            <Link to="/">
+              <span className="mr-6">صفحه اصلی</span>
+            </Link>
+          </div>
         </div>
       </>
     );
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,8 +2,10 @@ import Navbar from "../component/shared/Navbar";
 
 export default function HomePage() {
     return (
-        <>
-        <Navbar/>
-        </>
-    )
+      <>
+        <div className="bg-[url('src/assets/Frame 1.jpg')] bg-center bg-cover bg-no-repeat h-screen w-full  ">
+          <Navbar />
+        </div>
+      </>
+    );
 }
