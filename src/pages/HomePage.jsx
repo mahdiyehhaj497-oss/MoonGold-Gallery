be44f8fa023ce26147a527ef8d8c7cbe9e1665bf
mahdiyeh <mahdiyehhaj497@gmail.com>
2026-07-26 00:32:11 +0330

    add color

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,7 +6,13 @@ export default function HomePage() {
         <div className=" pt-6">
           <Navbar />
 
-          <div className="bg-[url('public/pexels-karola-g-6276048.jpg')] bg-no-repeat bg-center bg-cover h-screen mx-6 rounded-2xl"></div>
+          <div className="bg-[url('public/pexels-pham-ngoc-anh-170983008-28146843.jpg')] bg-no-repeat bg-center bg-cover h-screen pt-16 mx-5 rounded-2xl">
+            <div className="flex justify-center items-center">
+              <h1 className="font-semibold  text-lg pt-10">
+                درخشیدن به سبک ماه
+              </h1>
+            </div>
+          </div>
         </div>
       </>
     );
